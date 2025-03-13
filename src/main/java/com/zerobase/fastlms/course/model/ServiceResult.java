6d7cbcd5dc67634de1d155d9f5d48694d0206d36
musqat <hjs90561@naver.com>
2025-03-14 05:50:21 +0900

    강좌 신청 구현

---
+++ src/main/java/com/zerobase/fastlms/course/model/ServiceResult.java
@@ -0,0 +1,10 @@
+package com.zerobase.fastlms.course.model;
+
+import com.zerobase.fastlms.admin.model.CommonParam;
+import lombok.Data;
+
+@Data
+public class ServiceResult {
+  boolean result;
+  String message;
+}
